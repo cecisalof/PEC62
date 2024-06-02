bf74e4c33532dc4b9d857eaf2ec28a5139bcad8e
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2024-06-02 19:59:35 +0200

    [Fix]: file imports

--- PEC5_Ej_Prac/ecommerce/src/app/app.component.ts
+++ PEC5_Ej_Prac/ecommerce/src/app/app.component.ts
@@ -1,14 +1,24 @@
-/* Archivos relacionados con el componente principal de la aplicación. */ 
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ArticleItemModule } from './article-item/article-item.module';
 import { ArticleListModule } from './article-list/article-list.module';
-
+import { NavbarModule } from './navbar/navbar.module';
+import { ArticleNewTemplateModule } from './article-new-template/article-new-template.module'
+import { ArticleNewReactiveModule } from './article-new-reactive/article-new-reactive.module'
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ArticleItemModule, ArticleListModule],
+  imports: [CommonModule,
+    RouterOutlet,
+    ArticleItemModule,
+    ArticleListModule,
+    NavbarModule,
+    FormsModule,
+    ArticleNewTemplateModule,
+    ArticleNewReactiveModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
