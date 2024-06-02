d518a98de88a2fe0673e3d7ded8f4cd02d447233
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2024-06-02 19:57:40 +0200

    [Feature]: NavBar

---
+++ PEC5_Ej_Prac/ecommerce/src/app/navbar/navbar.module.ts
@@ -0,0 +1,13 @@
+// navbar.module.ts
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar.component';
+import {ArticleListModule } from '../article-list/article-list.module';
+
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [CommonModule, ArticleListModule],
+  exports: [NavbarComponent],
+})
+export class NavbarModule {}
