71ea431a392348b1e2395ee73210d0acf603a872
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2024-06-02 19:58:56 +0200

    [Routes]: routes

--- PEC5_Ej_Prac/ecommerce/src/app/app.routes.ts
+++ PEC5_Ej_Prac/ecommerce/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleNewTemplateComponent } from './article-new-template/article-new-template.component';
+import { ArticleNewReactiveComponent } from './article-new-reactive/article-new-reactive.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/articles', pathMatch: 'full' },
+  { path: 'articles', component: ArticleListComponent },
+  { path: 'new-template', component: ArticleNewTemplateComponent },
+  { path: 'new-reactive', component: ArticleNewReactiveComponent },
+];
