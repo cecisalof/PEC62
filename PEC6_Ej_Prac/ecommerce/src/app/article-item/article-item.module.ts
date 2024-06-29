aeee6fe3b36f555d820e3c5f2ed94e2ed6be3d41
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2024-06-29 15:27:39 +0200

    [Feature]: ImageArticlePipe

--- PEC6_Ej_Prac/ecommerce/src/app/article-item/article-item.module.ts
+++ PEC6_Ej_Prac/ecommerce/src/app/article-item/article-item.module.ts
@@ -3,9 +3,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleItemComponent } from './article-item.component';
+import { ImageArticlePipe } from '../pipes/image-article-pipe';
 
 @NgModule({
-  declarations: [ArticleItemComponent],
+  declarations: [ArticleItemComponent, ImageArticlePipe],
   imports: [CommonModule],
   exports: [ArticleItemComponent, CommonModule],
 })
---
+++ PEC6_Ej_Prac/ecommerce/src/app/pipes/image-article-pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'imageArticle'
+})
+
+export class ImageArticlePipe implements PipeTransform {
+  private defaultImageUrl:string ='assets/images/article2.png'
+  transform(imageUrl: string): string {    
+    if (imageUrl === '') {
+      return this.defaultImageUrl;
+    }
+    return imageUrl;
+  }
+}
