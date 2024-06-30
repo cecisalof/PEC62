731d7a1c92beb7b05016075f5be3195f68b8b70a
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2024-06-30 23:08:43 +0200

    [Fix]: routes

--- PEC6_Ej_Prac/ecommerce/src/app/app.routes.ts
+++ PEC6_Ej_Prac/ecommerce/src/app/app.routes.ts
@@ -2,17 +2,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ArticleListComponent } from './article-list/article-list.component';
-import { ArticleNewTemplateComponent } from './article-new-template/article-new-template.component';
+// import { ArticleNewTemplateComponent } from './article-new-template/article-new-template.component';
 import { ArticleNewReactiveComponent } from './article-new-reactive/article-new-reactive.component';
 import { RegisterComponent } from "./user/register/register.component";
 import { LoginComponent } from "./user/login/login.component";
+// import { AuthGuard } from './guards/auth.guard';
+
 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent },
-  { path: 'articles', component: ArticleListComponent },
-  { path: 'new-template', component: ArticleNewTemplateComponent },
-  { path: 'new-reactive', component: ArticleNewReactiveComponent },
+  { path: 'article/list', component: ArticleListComponent },
+  // { path: 'new-template', component: ArticleNewTemplateComponent },
+  { path: 'article/create', component: ArticleNewReactiveComponent},
 ];
